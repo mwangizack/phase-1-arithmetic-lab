496d892f132f2166b7386b49885e6dd96c5799db
Zacchaeus Ndirangu Mwangi <ndirangu.zmwangi@gmail.com>
2024-03-25 22:46:05 +0300

    Create variables assigned to math expressions

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 
 // Write your code here
+let num1 = 31, num2 = 2;
+const multiply = num1 * num2;
+
+let random = Math.ceil(Math.random());
+
+let num3 = 16, num4 = 6;
+let mod = num3 % num4;
+
+let max = Math.max(2,7,10.5,18,20,16);
